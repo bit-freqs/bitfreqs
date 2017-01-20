f147ae7f49aa3699cf39cdb9051af5ce3b39a9c2
Piet Geursen <pietgeursen@gmail.com>
2017-01-20 19:01:39 +1300

    Remove react stuff.

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/app'
-
-render(<App name='bitFreqs' />, document.querySelector('main'))
 console.log('welcome to bitFreqs')
