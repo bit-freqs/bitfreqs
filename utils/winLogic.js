9ee7eeef235cd4fdb7eb10ea5bdc1ee1d7b71192
gabrielamelian <gabrielamelian@gmail.com>
2017-01-21 15:19:53 +1300

    Win logic working, tested ok (manually)

--- utils/winLogic.js
+++ utils/winLogic.js
@@ -4,15 +4,16 @@ module.exports = {
 }
 
 function pickedAllCoins (totalCoins, coinsPicked){
-  if (totalCoins === coinsPicked){
+  if (totalCoins == coinsPicked){
     return true
   }
   return false
 }
 
 function checkIfWin (gameWidth, playerX, totalCoins, coinsPicked){
-  var playerAtEndOfScreen = gameWidth == playerX
+  var playerAtEndOfScreen = playerX >= gameWidth - 20
   var playerPickedAllCoins = pickedAllCoins(totalCoins, coinsPicked)
+
   if (playerAtEndOfScreen && playerPickedAllCoins){
     console.log("WINNER")
     return "Wins game"
