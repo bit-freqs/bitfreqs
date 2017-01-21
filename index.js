de8f089e2d8e211681f582673d3d9e89ae8ccfd1
Piet Geursen <pietgeursen@gmail.com>
2017-01-21 15:27:02 +1300

    Can show splash, play and gameover screens

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ var Coin = require('./coin')
 var Box = require('./box')
 var audio = require('./audio')
 
-pull(
-  audio(),
-  pull.log()
-)
 
 var grid = require('./utils/grid')
 var updateModule = require('./update')
@@ -22,63 +18,114 @@ var gameWidth = 1200
 var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
-    game.load.image('block', 'assets/block.png');
-    game.load.image('background', 'assets/background2.png');
-    game.load.spritesheet('coin', 'assets/sprite-coin.png', 32, 32);
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  game.load.image('block', 'assets/block.png');
+  game.load.image('background', 'assets/background2.png');
+  game.load.spritesheet('coin', 'assets/sprite-coin.png', 32, 32);
+  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
-var sprite;
-var updateParameters = {}
+function setState(game, action) {
+  switch(action.type){
+    case 'SPLASH_SCREEN': 
+      showSplash()
+      break;
+    case 'START': 
+      startGame()
+      break;
+    case 'GAME_OVER':
+      showGameover()
+      break;
+  } 
+}
+
+var updateParameters = {};
+
 function create() {
-    var bg = game.add.tileSprite(0, 0, gameWidth, gameHeight, 'background');
-
-    //  Enable p2 physics
-    game.physics.startSystem(Phaser.Physics.P2JS);
-    game.physics.p2.gravity.y = 2500;
-    game.physics.p2.world.defaultContactMaterial.friction = 0.3;
-    game.physics.p2.world.setGlobalStiffness(1e5);
-
-    //  Add a sprite
-    var player = game.add.sprite(0, gameHeight - 100, 'dude');
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('turn', [4], 20, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-    //  Enable if for physics. This creates a default rectangular body.
-    game.physics.p2.enable(player);
-
-    player.body.fixedRotation = true;
-    player.body.damping = 0.5;
-
-
-    var boxPlacer = Box(game)
-    for (var location of grid.boxLocations) {
-        boxPlacer.place(location.y, location.x)
-    }
-
-    var coinPlacer = new Coin(game);
-    for (var location of grid.coinLocations) {
-        var coin = coinPlacer.place(location.y, location.x)
-    }
-
-    //  Here is the contact material. It's a combination of 2 materials, so whenever shapes with
-    //  those 2 materials collide it uses the following settings.
-    var spriteMaterial = game.physics.p2.createMaterial('spriteMaterial', player.body);
-    var groundPlayerCM = game.physics.p2.createContactMaterial(spriteMaterial, AbstractGrid.worldMaterial, { friction: 0.0 });
-    var groundBoxesCM = game.physics.p2.createContactMaterial(AbstractGrid.worldMaterial, AbstractGrid.material, { friction: 0.6 });
-
-    var cursors = game.input.keyboard.createCursorKeys();
-    var jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-    updateParameters = {
-        player: player,
-        cursors: cursors,
-        jumpButton: jumpButton,
-        game: game
-    }
+  var bg = game.add.tileSprite(0, 0, gameWidth, gameHeight, 'background');
+  setState(game, {type: 'GAME_OVER'})
+  //setState(game, {type: 'START'}) //FOR DEV, SKIP SPLASH
 }
 
 function update() {
-    return updateModule(updateParameters)
+  return updateModule(updateParameters, setState)
+}
+
+function showSplash() {
+
+  var playLabel = addCenteredText(game, 'Play!')
+  playLabel.inputEnabled = true;
+  playLabel.events.onInputUp.add( () => {
+    playLabel.destroy()
+    setState(game, {type: 'START'} )
+  })
+}
+
+
+function showGameover() {
+  var deadLabel = addCenteredText(game, 'He Dead', {offsetY: -90})
+  var playLabel = addCenteredText(game, 'Play again!')
+  playLabel.inputEnabled = true;
+  playLabel.events.onInputUp.add( () => {
+    playLabel.destroy()
+    setState(game, {type: 'START'} )
+  })
+}
+
+function addCenteredText(game, text, opts) {
+  opts = opts || {}
+  opts = Object.assign({ font: '100px Arial', fill: '#fff', offsetX: 0, offsetY: 0 }, opts)
+  var label = game.add.text(0, 0, text, opts)
+  label.x = (gameWidth / 2) - label.width / 2 + opts.offsetX
+  label.y = gameHeight / 2 + opts.offsetY
+  return label
+}
+
+function startGame() {
+  var bg = game.add.tileSprite(0, 0, gameWidth, gameHeight, 'background');
+
+  //  Enable p2 physics
+  game.physics.startSystem(Phaser.Physics.P2JS);
+  game.physics.p2.gravity.y = 2500;
+  game.physics.p2.world.defaultContactMaterial.friction = 0.3;
+  game.physics.p2.world.setGlobalStiffness(1e5);
+
+  //  Add a sprite
+  var player = game.add.sprite(0, gameHeight - 100, 'dude');
+  player.animations.add('left', [0, 1, 2, 3], 10, true);
+  player.animations.add('turn', [4], 20, true);
+  player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+  //  Enable if for physics. This creates a default rectangular body.
+  game.physics.p2.enable(player);
+
+  player.body.fixedRotation = true;
+  player.body.damping = 0.5;
+
+
+  var boxPlacer = Box(game)
+  for (var location of grid.boxLocations) {
+    boxPlacer.place(location.y, location.x)
+  }
+
+  var coinPlacer = new Coin(game);
+  for (var location of grid.coinLocations) {
+    var coin = coinPlacer.place(location.y, location.x)
+  }
+
+  //  Here is the contact material. It's a combination of 2 materials, so whenever shapes with
+  //  those 2 materials collide it uses the following settings.
+  var spriteMaterial = game.physics.p2.createMaterial('spriteMaterial', player.body);
+  var groundPlayerCM = game.physics.p2.createContactMaterial(spriteMaterial, AbstractGrid.worldMaterial, { friction: 0.0 });
+  var groundBoxesCM = game.physics.p2.createContactMaterial(AbstractGrid.worldMaterial, AbstractGrid.material, { friction: 0.6 });
+
+  var cursors = game.input.keyboard.createCursorKeys();
+  var jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+  updateParameters = {
+    player: player,
+    cursors: cursors,
+    jumpButton: jumpButton,
+    game: game
+  }
+
 }
--- update.js
+++ update.js
@@ -1,75 +1,79 @@
 
 var state = {
-    facing: "left",
-    jumpTimer: 0
+  facing: "left",
+  jumpTimer: 0
 }
 
-module.exports = function update(updateParameters) {
-    var game = updateParameters.game;
-    var player = updateParameters.player;
-    var cursors = updateParameters.cursors;
-    var jumpButton = updateParameters.jumpButton;
-    var velocityAbs = 300;
+module.exports = function update(updateParameters, setState) {
+  var game = updateParameters.game;
 
-    player.body.velocity.x = 0;
+  if(updateParameters.player){
+    updatePlayer(updateParameters)
+  }
+}
 
-    if (cursors.left.isDown) {
-        player.body.velocity.x = -velocityAbs;
+function updatePlayer({player, game, cursors, jumpButton}){
+  var velocityAbs = 300;
 
-        if (state.facing != 'left') {
-            player.animations.play('left');
-            state.facing = 'left';
-        }
-    } else if (cursors.right.isDown) {
-        player.body.velocity.x = velocityAbs;
+  player.body.velocity.x = 0;
 
-        if (state.facing != 'right') {
-            player.animations.play('right');
-            state.facing = 'right';
-        }
-    } else {
-        if (state.facing != 'idle') {
-            player.animations.stop();
+  if (cursors.left.isDown) {
+    player.body.velocity.x = -velocityAbs;
 
-            if (state.facing == 'left') {
-                player.frame = 0;
-            } else {
-                player.frame = 5;
-            }
-
-            state.facing = 'idle';
-        }
+    if (state.facing != 'left') {
+      player.animations.play('left');
+      state.facing = 'left';
     }
+  } else if (cursors.right.isDown) {
+    player.body.velocity.x = velocityAbs;
 
-    if (isJumping(jumpButton, cursors) && checkIfCanJump(game, player) 
-            && game.time.now > state.jumpTimer) {
-        player.body.velocity.y = -700 
-        state.jumpTimer = game.time.now + 750
+    if (state.facing != 'right') {
+      player.animations.play('right');
+      state.facing = 'right';
     }
+  } else {
+    if (state.facing != 'idle') {
+      player.animations.stop();
+
+      if (state.facing == 'left') {
+        player.frame = 0;
+      } else {
+        player.frame = 5;
+      }
+
+      state.facing = 'idle';
+    }
+  }
+
+  if (isJumping(jumpButton, cursors) && checkIfCanJump(game, player) 
+    && game.time.now > state.jumpTimer) {
+      player.body.velocity.y = -700 
+      state.jumpTimer = game.time.now + 750
+  }
 }
 
 function isJumping(jumpButton, cursors) {
-    return jumpButton.isDown || cursors.up.isDown;
+  return jumpButton.isDown || cursors.up.isDown;
 }
 
 var yAxis = p2.vec2.fromValues(0, 1);
 function checkIfCanJump(game, player) {
-    var result = false;
-    for (var i=0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++) {
-        var c = game.physics.p2.world.narrowphase.contactEquations[i];
+  var result = false;
+  for (var i=0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++) {
+    var c = game.physics.p2.world.narrowphase.contactEquations[i];
 
-        if (c.bodyA === player.body.data || c.bodyB === player.body.data) {
-            var d = p2.vec2.dot(c.normalA, yAxis);
+    if (c.bodyA === player.body.data || c.bodyB === player.body.data) {
+      var d = p2.vec2.dot(c.normalA, yAxis);
 
-            if (c.bodyA === player.body.data) {
-                d *= -1;
-            }
+      if (c.bodyA === player.body.data) {
+        d *= -1;
+      }
 
-            if (d > 0.5) {
-                result = true;
-            }
-        }
+      if (d > 0.5) {
+        result = true;
+      }
     }
+  }
 
-    return result;
+  return result;
 }
