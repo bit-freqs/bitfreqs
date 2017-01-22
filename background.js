d60a06854e7abaa01858bf2ee3af12db4254f2b7
gabrielamelian <gabrielamelian@gmail.com>
2017-01-22 14:21:37 +1300

    Modifies scale for new waves

--- background.js
+++ background.js
@@ -3,23 +3,23 @@ var {gameHeight, gameWidth} = require('./config')
 module.exports = {
   preload: function (game) {
     game.load.image('background', 'assets/background-flat.png')
-    game.load.spritesheet('waves', 'assets/wave-sprite-attempt.png', 446.2, 50, 5)
+    game.load.spritesheet('waves', 'assets/wave-sprite-attempt.png', 1366, 150, 5)
   },
   create: function (game) {
     var bg = game.add.image(0, 0, 'background')
     this.createWaves(game)
   },
   createWaves: function (game) {
-    var scale = 1
+    var scale = 0.7
     //var waves = game.add.sprite(0, gameHeight - (32 * scale), 'waves')
     //waves.animations.add('animateWaves', [4, 3, 2, 1, 0], 5, true)
     //waves.animations.play('animateWaves')
     //waves.scale.setTo(scale, scale)
 
     var waves = []
-    waves.push(game.add.sprite(0, gameHeight - (50 * scale), 'waves'))
-    waves.push(game.add.sprite(446.2 * scale, gameHeight - (50 * scale), 'waves'))
-    waves.push(game.add.sprite(892.4 * scale, gameHeight - (50 * scale), 'waves'))
+    waves.push(game.add.sprite(0, gameHeight - (150 * scale), 'waves'))
+    waves.push(game.add.sprite(1366 * scale, gameHeight - (150 * scale), 'waves'))
+    // waves.push(game.add.sprite(892.4 * scale, gameHeight - (150 * scale), 'waves'))
 
     for (var wave of waves) {
       wave.animations.add('animateWaves', [4, 3, 2, 1, 0], 5, true)
