0725ef3771b82017dc3d5c721054371ce47622b8
gabrielamelian <gabrielamelian@gmail.com>
2017-01-22 14:34:35 +1300

    Fix waves to 6 sprites

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ var {gameHeight, gameWidth} = require('./config')
 module.exports = {
   preload: function (game) {
     game.load.image('background', 'assets/background-flat.png')
-    game.load.spritesheet('waves', 'assets/wave-sprite-attempt.png', 1366, 150, 5)
+    game.load.spritesheet('waves', 'assets/wave-sprite-attempt.png', 1138.33, 150, 5)
   },
   create: function (game) {
     var bg = game.add.image(0, 0, 'background')
@@ -18,11 +18,11 @@ module.exports = {
 
     var waves = []
     waves.push(game.add.sprite(0, gameHeight - (150 * scale), 'waves'))
-    waves.push(game.add.sprite(1366 * scale, gameHeight - (150 * scale), 'waves'))
+    waves.push(game.add.sprite(1138.33 * scale, gameHeight - (150 * scale), 'waves'))
     // waves.push(game.add.sprite(892.4 * scale, gameHeight - (150 * scale), 'waves'))
 
     for (var wave of waves) {
-      wave.animations.add('animateWaves', [4, 3, 2, 1, 0], 5, true)
+      wave.animations.add('animateWaves', [0, 1, 2, 3 ,4, 5], 5, true)
       wave.animations.play('animateWaves')
       wave.scale.setTo(scale, scale)
     }
